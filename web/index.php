80fabb3a3d75a76d791722548d2093d4d2a55cb7
wuding <netjoin@gmail.com>
2019-05-18 21:48:19 +0800

    添加多域名定义和文件类型输出、自定义超文本标记语言标签名支持

--- web/index.php
+++ web/index.php
@@ -18,11 +18,28 @@
 $config = include __DIR__ . '/../app/config/autoload.php';
 # print_r($config);
 foreach ($config as $file) {
-	foreach ($file['filename'] as $filename) {
-		$filepath = $file['prefix'] . $filename . $file['suffix'];
-		include $filepath;
-	}
+    foreach ($file['filename'] as $filename) {
+        $filepath = $file['prefix'] . $filename . $file['suffix'];
+        include $filepath;
+    }
 }
+
+/**
+ * 配置
+ */
+$hosts = array(
+        'composer.urlnk.host' => 'D:/env\www\work\wubenli\composer',
+        'urlnk.cc' => 'D:/env\www\work\netjoin\cdn',
+    );
+$contentType = array(
+        'css' => 'text/css',
+    );
+
+/**
+ * 请求
+ */
+$html_tag = isset($_GET['html_tag']) ? $_GET['html_tag'] : '';
+
 
 /**
  * 对象变量定义
@@ -30,7 +47,9 @@ foreach ($config as $file) {
 $directory = new Ext\Directory();
 $filesystem = new Ext\Filesystem();
 $pathinfo = $_SERVER['PATH_INFO'];
-$directory_prefix = 'E:\www\work\wubenli\composer' . $pathinfo;
+$http_host = $_SERVER['HTTP_HOST'];
+$filename_ext = pathinfo($pathinfo, PATHINFO_EXTENSION);
+$directory_prefix = $hosts[$http_host] . $pathinfo;
 
 /**
  * 输出内容
@@ -45,7 +64,11 @@ if (is_dir($directory_prefix)) {
     }
 } elseif (is_file($directory_prefix)) {
     $str = Ext\Filesystem::getContents($directory_prefix);
-    $str = "<pre>$str</pre>";
+    if ($html_tag) {
+        $str = "<$html_tag>$str</$html_tag>";
+    } elseif ($filename_ext && array_key_exists($filename_ext, $contentType)) {
+        header("Content-Type: $contentType[$filename_ext]");
+    }
 }
 
 echo $str;
