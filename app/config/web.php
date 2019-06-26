d02e48f35c5da5ee083b16478e55eb7a5fff2b7f
wuding <netjoin@gmail.com>
2019-06-26 22:50:15 +0800

    新增 Web 配置文件

---
+++ app/config/web.php
@@ -0,0 +1,10 @@
+<?php
+return  array(
+    'hosts' => array(
+        'composer.urlnk.host' => 'D:/env\www\work\wubenli\composer',
+        'urlnk.cc' => 'D:/env\www\work\netjoin\cdn\web',
+    ),
+    'contentType' => array(
+        'css' => 'text/css',
+    ),
+);
